da22f038de43de71a92ceff63818d655a3186fb1
andrei-s24 <andrei2002.as@gmail.com>
2023-01-30 16:12:24 +0000

    Added CSRF token to React form

--- wireless_signal_power_calculator/ui/src/App.js
+++ wireless_signal_power_calculator/ui/src/App.js
@@ -7,14 +7,37 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
 function App() {
   return (
     <div className="App">
       <Row>
         <Col>
-          <Form method="POST">
+          <Form onSubmit={e => {
+            e.preventDefault();
+            let data = new FormData();
+            // add form input from hidden input elsewhere on the page
+            fetch("", {
+              method: 'POST',
+              body: data,
+              headers: { "X-CSRFToken": '{{csrf_token}}' }
+            })
+          }}>
             {/* CSRF Token not working */}
             <h1> Inputs </h1>
             <br />
